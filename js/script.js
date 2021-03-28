b38d61ec2908b292bf9031e43117c8e006e34856
Lakeend <78341971+Lakeend@users.noreply.github.com>
2021-03-29 01:32:07 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 'use strict';
-/* Menu */
 const menuBtn = document.querySelector('.header__bottom-menu-btn'),
       menuContent = document.querySelector('.header__bottom-menu-wrap'),
       topContent = document.querySelector('.header__top');
-menuBtn.addEventListener('click', function() {
-    menuBtn.classList.toggle('active');
+menuBtn.addEventListener('click', function () {
+    this.classList.toggle('active')
     menuContent.classList.toggle('active');
     topContent.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+})
+function closeModal() {
+    menuBtn.classList.remove('active')
+    menuContent.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+    topContent.classList.remove('active');
+}
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && menuContent.classList.contains('active')) { 
+        closeModal();
+    }
 });
-
-/* Menu END*/
+document.addEventListener('click', (e) => {
+   const modal = doument.querySelectorAll('.active);
+   modal.forEach(item => {
+      if(e.target !== item || e.target !== item.children) {
+    closeModal();
+   }                                
+   });
+});
